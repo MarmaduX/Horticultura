294f1afa2a023c53bf92f67366c3944498562189
camilaaraujo06 <90867638+camilaaraujo06@users.noreply.github.com>
2021-10-13 16:39:21 -0300

    Cree carpeta web
    Aquí están todos los cultivos

---
+++ web/servicios/autenticacion.py
@@ -0,0 +1,67 @@
+import requests
+
+from web.servicios import rest_api
+
+def valirdar_credenciales(usuario, clave):
+    body = {"nombre": usuario,
+           "clave": clave}
+    respuesta = requests.post(f'{rest_api.API_URL}/login', json=body)
+    return = respuesta.status_code == 200
+
+
+def crear_usuario(usuario, clave):
+    body = {"nombre": usuario,
+            "clave": clave}
+    respuesta = requests.post(f'{rest_api.API_URL}/usuarios', json=body)
+    return respuesta.status_code == 200
+
+
+def obtener_usuarios():
+    respuesta = requests.get(f'{rest_api.API_URL}/usuarios')
+    return respuesta.json()
+
+def crear_cultivo():
+    body = {"nombreCientífico": nombreCientífico,
+            "tipoCultivo": tipoCultivo,
+            "descripción": descripción,
+            "plagas": plagas,
+            "enfermedades": enfermedades}
+    respuesta = request.post(f'{rest_api.API_URL}/cultivos', json=body)
+    return respuesta.status_code == 200
+
+def modificar_cultivo():
+    body = {"nombreCientífico": nombreCientífico,
+            "tipoCultivo": tipoCultivo,
+            "descripción": descripción,
+            "plagas": plagas,
+            "enfermedades": enfermedades}
+    respuesta = request.put(f'{rest_api.API_URL}/cultivos', json=body)
+    return respuesta.status_code == 200
+
+def listar_cultivos():
+    body = {"nombreCientífico": nombreCientífico,
+            "tipoCultivo": tipoCultivo,
+            "descripción": descripción,
+            "plagas": plagas,
+            "enfermedades": enfermedades}
+    respuesta = request.get(f'{rest_api.API_URL}/cultivos', json=body)
+    return respuesta.status_code == 200
+
+def mostrar_cultivos():
+    body = {"nombreCultivo": nombreCultivo,
+            "tipoCultivo": tipoCultivo}
+    respuesta = request.get(f'{rest_api.API_URL}/cutlivos', json=body)
+    return respuesta.status_code == 200
+
+def mostrar_cultivo_por():
+    body = {"nombreCientífico": nombreCientífico,
+            "tipoCultivo": tipoCultivo,
+            "descripción": descripción,
+            "plagas": plagas,
+            "enfermedades": enfermedades}
+    respuesta = request.get(f'{rest_api.API_URL}/cultivos', json=body)
+    return respuesta.status_code == 200
+
+def eliminar_cultivo():
+    body = {}
+    respuesta = request.delete(f'{rest_api.API_URL}/cultivos')
